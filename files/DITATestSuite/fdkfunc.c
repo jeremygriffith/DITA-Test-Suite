92983bed8e0c6777c8a5d8229f482591e4e3d15b
jeremygriffith <jeremy@omsys.com>
2014-02-19 13:30:08 -0500

    zip content

---
+++ files/DITATestSuite/fdkfunc.c
@@ -0,0 +1,17 @@
+﻿// fdkfunc.c has a small code example
+// Unicode: Mit freundlichen Grüßen
+
+VoidT RunDOSCommand(StringT cmd, BoolT hide, BoolT keep)
+{
+	StringT command = NULL;
+
+	command = F_StrNew(F_StrLen(ComSpec) + F_StrLen(cmd) + 5);
+	F_StrCpy(command, ComSpec);
+	F_StrCat(command, keep ? " /k " : " /c ");
+	F_StrCat(command, cmd);
+
+	RunCommand(command, CurrDir, True, hide, False);
+	F_StrFree(command);
+}
+
+/* end of fdkfunc.c */
